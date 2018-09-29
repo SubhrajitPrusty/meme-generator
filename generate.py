c1db1e8ba9e2e58c0c3288b8114c44bdf20e74fb
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-09-29 23:14:46 +0530

    added click, fixed algorithm to handle long texts

--- generate.py
+++ generate.py
@@ -1,31 +1,67 @@
-import sys
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import os
+import click
+import time
 
+@click.command()
+@click.argument("image", type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.argument("text", type=click.STRING)
+@click.option("--show", is_flag=True)
+def cli(image, text, show):
+	""" Generate a text + image meme """
 
-if len(sys.argv) < 2:
-	print("No arguments detected. Exiting.")
-else:
-	memetext = sys.argv[1]
-	img = sys.argv[2]
+	words = text.split(" ") # extract words
+	wordlen = [len(x) for x in words] # extract length of each words
 
-	mainImg = Image.open(img)
+	mainImg = Image.open(image)
 	width = mainImg.width
 	height = mainImg.height
+	
+	fontsize = int(height*0.05) # looks good enough
+	maxletters = (width//fontsize)*2 # adjustment because font size is weird
 
-	imText = Image.new('RGB', (width,int(height*0.15)), (255,255,255))
+	lines = []
+	i = k = 0
+	while i < len(words): # divide text into lines that can fit
+		line = []
+		s = 0
+		while k < len(words):
+			s+=len(words[k])
+			s+=1
+			if s < maxletters:
+				line.append(words[k])
+			else:
+				break
+			k+=1
+		lines.append(" ".join(line))
+		i=k
+
+	# print(lines)
+	# print([len(x) for x in lines])
+	# print(height, width, fontsize, maxletters)
+
+	lineheight = 1.5
+	textHeight = int((fontsize*lineheight)*len(lines)) # get total height of text
+
+	imText = Image.new('RGB', (width, textHeight), (255,255,255)) # make image space for text
 	draw = ImageDraw.Draw(imText)
 
-	font = ImageFont.truetype("Verdana.ttf",int(height*0.1))
+	font = ImageFont.truetype(os.path.join("fonts","Verdana.ttf"), fontsize) # select font
 
-	draw.text((20,int(imText.height*0.05)), memetext ,(0,0,0),font)
+	memetext = "\n".join(lines)
+	draw.text((fontsize//2, fontsize//2), memetext, (0,0,0), font)
 
-	total_h = height + imText.height
+	total_h = height + imText.height # total height of text + image
 
-	new_im = Image.new('RGB',(width,total_h),(255,255,255))
+	new_im = Image.new('RGB',(width,total_h),(255,255,255)) # make new image with size of text space + old image
 
-	new_im.paste(imText, (0,0))
-	new_im.paste(mainImg, (0,int(height*0.15)))
+	new_im.paste(imText, (0,0)) # paste text at top
+	new_im.paste(mainImg, (0,textHeight)) # paste image below text
 
-	new_im.save("meme.jpg")
+	if show:
+		new_im.show()
+	
+	new_im.save("meme-{}.jpg".format(int(time.time())))
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except:
+        pass
+
+setup(name = 'meme-generator',
+    version = '1.0',
+    description = 'Generate memes',
+    long_description=readme(),  
+    long_description_content_type='text/markdown', 
+
+    author='Subhrajit Prusty',  
+    author_email='subhrajit1997@gmail.com',
+    url = 'http://github.com/SubhrajitPrusty/meme-generator',
+
+    setup_requires=['setuptools>=40.0'],
+    
+    classifiers=[  
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='meme memes image', 
+    license = 'MIT',
+    packages=find_packages(), 
+    install_requires=['click', 'pillow'],
+    entry_points="""
+    [console_scripts]
+        memegen=generate:cli
+        """,
+)
